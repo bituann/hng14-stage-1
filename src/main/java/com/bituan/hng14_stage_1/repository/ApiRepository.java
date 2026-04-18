cbbe7873b8abebc1195dc78dec224cb78059c1d3
bituan <113036994+bituann@users.noreply.github.com>
2026-04-18 21:13:49 +0100

    write repository

--- src/main/java/com/bituan/hng14_stage_1/repository/ApiRepository.java
+++ src/main/java/com/bituan/hng14_stage_1/repository/ApiRepository.java
@@ -1,4 +1,12 @@
 package com.bituan.hng14_stage_1.repository;
 
-public interface ApiRepository {
+import com.bituan.hng14_stage_1.model.ApiResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ApiRepository extends JpaRepository<ApiResult, UUID> {
+    Optional<ApiResult> findByName (String name);
+    boolean existsByName (String name);
 }
