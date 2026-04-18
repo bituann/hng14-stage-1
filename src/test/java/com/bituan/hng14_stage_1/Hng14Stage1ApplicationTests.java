67cdd187b32c1c4435d5962ac0a166c8cb792327
bituan <113036994+bituann@users.noreply.github.com>
2026-04-18 21:15:08 +0100

    project files

---
+++ src/test/java/com/bituan/hng14_stage_1/Hng14Stage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bituan.hng14_stage_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hng14Stage1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
