18e77ca1c4118fe25e7708603d679148b5598eea
bituan <113036994+bituann@users.noreply.github.com>
2026-04-18 21:14:27 +0100

    main project class

---
+++ src/main/java/com/bituan/hng14_stage_1/Hng14Stage1Application.java
@@ -0,0 +1,13 @@
+package com.bituan.hng14_stage_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hng14Stage1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hng14Stage1Application.class, args);
+	}
+
+}
