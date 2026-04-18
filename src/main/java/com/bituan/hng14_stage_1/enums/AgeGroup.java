2ef3758e6675cf1de762c873aff8b62d0c4f6ff9
bituan <113036994+bituann@users.noreply.github.com>
2026-04-18 21:12:52 +0100

    write enums

--- src/main/java/com/bituan/hng14_stage_1/enums/AgeGroup.java
+++ src/main/java/com/bituan/hng14_stage_1/enums/AgeGroup.java
@@ -1,4 +1,12 @@
 package com.bituan.hng14_stage_1.enums;
 
 public enum AgeGroup {
+    child, teenager, adult, senior;
+
+    public static AgeGroup resolve (int age) {
+        if (age >= 60) return senior;
+        if (age >= 20) return adult;
+        if (age >= 13) return teenager;
+        return child;
+    };
 }
